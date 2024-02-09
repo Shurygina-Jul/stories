95c1af412f478be91478b4e1a93c6ddab7974d18
Shurygina Julia <melam.2916@gmail.com>
2024-02-10 00:10:00 +0300

    feat: переключение слайдов

---
+++ index.js
@@ -0,0 +1,34 @@
+//TODO: написать общую функцию
+
+document
+  .querySelector(".player-chunk-prev")
+  .addEventListener("click", function () {
+    function moveClass(className, activeClassName) {
+      let active = document.querySelector("." + activeClassName);
+      let prev = active && active.previousElementSibling;
+
+      if (prev && prev.classList.contains(className)) {
+        active.classList.remove(activeClassName);
+        prev.classList.add(activeClassName);
+      }
+    }
+
+    moveClass("timeline-chunk", "timeline-chunk-active");
+    moveClass("player-chunk", "player-chunk-active");
+  });
+
+document
+  .querySelector(".player-chunk-next")
+  .addEventListener("click", function () {
+    function moveClass(className) {
+      let active = document.querySelector("." + className);
+
+      if (active && active.nextElementSibling) {
+        active.classList.remove(className);
+        active.nextElementSibling.classList.add(className);
+      }
+    }
+
+    moveClass("timeline-chunk-active");
+    moveClass("player-chunk-active");
+  });
